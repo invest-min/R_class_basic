2222722155c3074fcbc524203eec5069a7d1c487
DESKTOP-425TVIL\user <invest.min@gmail.com>
2022-03-07 20:48:03 +0900

    Added *.Rproj to gitignore Created 00 class

---
+++ 00. Environment, Console, Package.R
@@ -0,0 +1,27 @@
+#Environment
+
+a <- 1
+b <- 2
+c <- 3
+
+ls()
+rm(a)
+ls()
+rm(list=ls()) #Console -> Ctrl+L
+ls()
+
+#Package
+
+install.packages("tidyverse")
+library(tidyverse)
+search()
+
+help(sum)
+help(package = "tidyverse")
+library(help = tidyverse)
+
+ls("package:tidyverse")
+data(package = "tidyverse")
+
+detach(package:tidyverse)
+search()
